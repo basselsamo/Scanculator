55e3a39d0e83a6335b60be6276d675135e650266
basselsamo <basselsamo@hotmail.com>
2025-01-27 02:31:52 +0100

    Initial Commit

---
+++ Scanculator/ScanculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanculatorApp.swift
+//  Scanculator
+//
+//  Created by Bassel Samo on 27.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ScanculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
